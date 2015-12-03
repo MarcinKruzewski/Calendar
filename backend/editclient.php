e5b28f53f0142f496f747bf915c66e3ad744f7b5
Marcin <kruzewski@klukowo.com>
2015-12-03 20:24:50 +0100

    successes fixes

--- backend/editclient.php
+++ backend/editclient.php
@@ -13,7 +13,8 @@ else
 ob_start();
 	print "<h1>Edycja danych klienta</h1><table><form method='post'>
 			<tr><th>Nazwa użytkownika:</th><td>{$client->getParam('user')}</td><td><input type='text' name='user'></td></tr>
-			<tr><th>Hasło:</th><td>********</td><td><input type='text' name='pswd'></td></tr>";
+			<tr><th>Hasło:</th><td>********</td><td><input type='password' name='pswd'></td></tr>
+			<tr><th>Powtórz hasło:</th><td>********</td><td><input type='password' name='pswd2'></td></tr>";
 if ($pswd['type']=='admin')
 {
 	print "	<tr><th>Typ:</th><td>{$client->getParam('type')}</td><td><select name='type'><option>Bez zmian</option><option>user</option><option>admin</option></td></tr>";
@@ -24,17 +25,53 @@ if ($pswd['type']=='admin')
 	print "<tr><td><label><input type='checkbox' name='delete' value='1' />Usuń klienta</label></td><td></td><td><input type='submit' value='Edytuj'></td></tr></table></form>";
 ob_end_flush();
 
-
-if (!empty($_POST['user'])) $client->setParam('user', $_POST['user']);
-if (!empty($_POST['pswd'])) $client->setParam('pswd', md5($_POST['pswd']));
-if (!empty($_POST['type'])&&$_POST['type']!='Bez zmian') $client->setParam('type', $_POST['type']);
+$changes = 0;
+if (!empty($_POST['user']))
+{
+	$client->setParam('user', $_POST['user']);
+	$changes ++;
+}
+if (!empty($_POST['pswd'])&&!empty($_POST['pswd2'])) 
+{
+	if ($_POST['pswd']==$_POST['pswd2'])
+	{
+		$client->setParam('pswd', md5($_POST['pswd']));
+		$changes ++;
+	}
+	else
+	{
+		print "Hasła nie są identyczne!";
+	}
+}
+if (!empty($_POST['type'])&&$_POST['type']!='Bez zmian') 
+{
+	$client->setParam('type', $_POST['type']);
+	$changes ++;
+}
 if (!empty($_POST['style']))
 {
 	if ($_POST['style']=='grey') $client->setparam('style', 1);
 	if ($_POST['style']=='blue') $client->setparam('style', 2);
 	if ($_POST['style']=='pink') $client->setparam('style', 3);
+	$changes ++;
 } 
-if (!empty($_POST['mail'])) $client->setParam('mail', $_POST['mail']);
-if (!empty($_POST['tel'])) $client->setParam('tel', $_POST['tel']);
-if (!empty($_POST['delete'])) $client->delete();
-$client->replace();
+if (!empty($_POST['mail'])) 
+{
+	$client->setParam('mail', $_POST['mail']);
+	$changes ++;
+}
+if (!empty($_POST['tel'])) 
+{
+	$client->setParam('tel', $_POST['tel']);
+	$changes ++;
+}
+if (!empty($_POST['delete'])) 
+{
+	$client->delete();
+	$changes ++;
+}
+if ($changes > 0)
+{
+	$client->replace();
+	header('Location: 8.html');
+}
